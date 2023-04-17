d154241fcd2ae1ba16e25bf661d8c49c29341487
Anjaraniaina <hei.anjaraniaina@gmail.com>
2023-04-17 14:11:58 +0300

    feat: add readme

---
+++ src/test/java/com/codelike/mozart/MozartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codelike.mozart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MozartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
